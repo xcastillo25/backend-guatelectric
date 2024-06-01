6ff6c12ca782ec4e09c9db422f759eed33d4450a
xcastillo25 <xcastillo25@gmail.com>
2024-06-01 12:12:26 -0600

    Ultima

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ app.use((err, req, res, next) => {
 
 // Cabeceras CORS
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+    
+    // res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.header('Access-Control-Allow-Origin', 'http://dy491gmo7el38.cloudfront.net');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
